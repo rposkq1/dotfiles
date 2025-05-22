83b5eb6385c692353199527cbece75e58c003530
rposkq1 <rposkq@proton.me>
2025-05-22 19:51:49 +0200

    remove X variable from read

--- move_dotfiles_to_home.sh
+++ move_dotfiles_to_home.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-read -p "will move .xinitrc  .xbindkeysrc  .monitors  .config  .bashrc  .bash_profile" X
+read -p "will move .xinitrc  .xbindkeysrc  .monitors  .config  .bashrc  .bash_profile"
 mv .xinitrc .xbindkeysrc .monitors .config .bashrc .bash_profile ~/
