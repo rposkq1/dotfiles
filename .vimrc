ae48cfd9064059ce405d34726491500365dce926
rposkq1 <rposkq@proton.me>
2025-07-28 13:37:19 +0200

    remove old configs

--- .vimrc
+++
@@ -1,3 +0,0 @@
-syntax on
-set background=dark
-colorscheme wildcharm
--- move_dotfiles_to_home.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-read -p "will move .xinitrc  .xbindkeysrc  .monitors  .config  .bashrc  .bash_profile"
-mv .xinitrc .xbindkeysrc .monitors .config .bashrc .bash_profile ~/
